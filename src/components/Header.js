d5eae7b4cf09b13510b85983b48df5bfe23d91ee
shubhiupa19 <s.x.upadhyay@gmail.com>
2023-11-08 22:32:19 -0500

    basically done tbh

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+
 export default function Header() {
     return (
         <header className="bg-white bg-opacity-30 text-white">
         <nav className="container mx-auto py-3 flex justify-between items-center">
-          <a href="/" className="text-white text-3xl font-bold hover:text-gray-300">Logo</a>
+          <Image src = "/images/logo.svg" alt="Logo" width={50} height={50}/>
           <div className="flex justify-center flex-grow space-x-20 py-2">
             <a href="/play" className="hover:text-gray-300 font-semibold text-2xl">PLAY</a>
             <a href="/friends" className="hover:text-gray-300 font-semibold text-2xl">FRIENDS</a>
--- src/components/TableListComponent.js
+++ src/components/TableListComponent.js
@@ -1,13 +1,28 @@
+import Image from 'next/image';
 const TableListComponent = ({ tables }) => {
 
     return (
         <div className = "my-8">
             {tables.map((table, index) => (
-                <div key = {index} className = "bg-tablegreen shadow-lg rounded-lg p-4 mb-4 text-black w-1/2"> 
-                <h2 className = "font-semibold text-white">{table.name}</h2>
-                <p className = "text-white">Blinds: {table.blinds}</p>
-                <p className = "text-white">Players: {table.players} / {table.maxPlayers}</p>
-                <p className = "text-white">Duration: {table.duration}</p>
+                <div key = {index} className = "bg-tablegreen shadow-lg rounded-lg p-4 mb-4 text-black w-1/2 flex justify-between items-center"> 
+                 <div className="flex flex-col">
+                <h2 className = "font-semibold text-white mb-1">{table.name}</h2>
+                <div className = "text-gray-300 mt-1 flex flex-row">
+                <div className="flex items-center mr-2"> 
+                <Image src="/images/Ellipse 3.svg" alt="Blinds" width={20} height={20} className="w-4 h-4 mr-1" /> 
+                <span>{table.blinds} </span>
+                </div>
+                <div className="flex items-center mr-2"> 
+                <Image src="/images/groups.svg" alt="Players" width={20} height={20} className="w-4 h-4 mr-1" /> 
+                <span className = "pr-2">{table.players}/{table.maxPlayers}</span>
+                </div>
+                <div className="flex items-center mr-2"> 
+                <Image src="/images/alarm.svg" alt="Duration" width={20} height={20} className="w-4 h-4 mr-1" /> 
+                <span className = "pr-2">{table.duration}</span>
+                </div>
+                </div>
+                </div>
+                <Image src="/images/arrow_back.svg" alt="Back" width={20} height={20} className="w-4 h-4 mr-1" />
                 </div>
             ))}
         </div>
