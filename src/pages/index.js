b2346faf5680e14c5d4b079aee4e93058c1d7949
shubhiupa19 <s.x.upadhyay@gmail.com>
2023-11-29 17:46:32 -0500

    home page almosttt there

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,43 +8,64 @@ export default function Home() {
   const [email, setEmail] = useState('');
   const handleLogin = async (event) => {
     event.preventDefault();
-
+   
+    //login 
     router.push('/TablesPage');
-
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-green-900 text-white p-4">
-      <div className="text-center mb-6">
-        <p className="text-sm uppercase tracking-widest">- Welcome to our Poker dApp -</p>
-        <h1 className="text-4xl font-bold mt-2">NYU Blockchain & Fintech</h1>
-        <p className="mt-2">NYU's community in blockchain & fintech</p>
+    <div className="flex h-screen background-color text-white">
+     
+      <div className="flex-grow flex flex-col justify-center items-start pl-20 space-y-6 max-w-lg">
+        <div className="text-left">
+          <p className="text-sm uppercase tracking-widest">- Welcome to our Poker dApp -</p>
+          <h1 className="text-4xl font-bold">NYU Blockchain & Fintech</h1>
+          <p className="mt-2">NYU's community in blockchain & fintech</p>
+        </div>
+        <form className="w-full" onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Enter your NYU email"
+            className="w-full px-4 py-2 text-white background-color"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="w-full bg-white text-black px-4 py-2 mt-4"
+          >
+            Log in
+          </button>
+        </form>
+        <Link href="/signup">
+          Don't have an account? Sign up
+        </Link>
       </div>
-      <form className="flex flex-col items-center w-full max-w-xs" onSubmit={handleLogin}>
-        <input 
-          type="email" 
-          placeholder="Enter your NYU email" 
-          className="w-full px-4 py-2 text-black"
+
+     
+      <div className="flex-grow flex justify-center items-center max-w-md">
+      <div className="ml-auto">
+        <Image
+          src="/images/Group 291.svg"
+          alt="Cards"
+          width={500}
+          height={500}
         />
-        <button 
-          type="submit" 
-          className="w-full bg-white text-black px-4 py-2 mt-4"
-        >
-          Log in
-        </button>
-      </form>
-      <Link href="/signup" className="text-white mt-4">
-        Don't have an account? Sign up
-      </Link>
-      <div className="absolute right-0 top-0 mt-10 mr-10">
-        <Image 
-          src="/images/playing_cards.svg" 
-          alt="Playing Cards" 
-          width={150} 
-          height={200} 
-          className="drop-shadow-2xl"
-        />
+        </div>
+      </div>
+
+      
+      <div className="absolute top-0 w-full bg-white bg-opacity-30">
+        <nav className="flex justify-between items-center p-4">
+          <Image
+            src="/images/logo.svg"
+            alt="Logo"
+            width={50}
+            height={50}
+          />
+          
+        </nav>
       </div>
     </div>
-  )
+  );
 }
