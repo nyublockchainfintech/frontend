702ba2d60f32b0c82b1592c4441ecf428b733eff
vaimathi1726 <149654601+vaimathi1726@users.noreply.github.com>
2023-12-05 21:47:10 -0500

    Vaikunthan code comments on Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 
+{/* calculates position of the player based on the index and the amount of players */}
 const Player = ({ name, avatar, money, index, totalPlayers }) => {
   const radiusX = 100;
   const radiusY = 70;
   const angle = (2 * Math.PI / totalPlayers) * index;
   const leftPosition = 50 + radiusX * Math.cos(angle);
   const topPosition = 50 + radiusY * Math.sin(angle);
-
+{/* defines the basic structure of how the player will appear */}
   const playerStyle = {
     position: 'absolute',
     left: `${leftPosition}%`,
@@ -15,7 +16,7 @@ const Player = ({ name, avatar, money, index, totalPlayers }) => {
     transform: 'translate(-50%, -50%)',
     zIndex: 1,
   };
-
+{/* returns the appearance of the player */}
   return (
     <div style={playerStyle}>
         <div style={{ position: 'relative' }}>
